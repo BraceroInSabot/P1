3b487bf5a8f8108fe84cc7a60b7836fe4d09a4a4
Guilherme Bracero Gonzales <guibragon@gmail.com>
2025-01-09 21:48:57 -0300

    add: Serializer model included on admin file

--- Main/apps/Save/admin.py
+++ Main/apps/Save/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Save
 
 # Register your models here.
+class SaveAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'total_amount', 'total_saved']
+    search_fields = ['name']
+    list_filter = ['created_at']
+    list_per_page = 10
+
+admin.site.register(Save, SaveAdmin)
