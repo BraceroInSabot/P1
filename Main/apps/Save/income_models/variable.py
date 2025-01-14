4861a3908a50bb3b4d0d8ee63b1020547a0c2fd8
Guilherme Bracero Gonzales <guibragon@gmail.com>
2025-01-14 03:28:44 -0300

    ref: DateAdded added in Variable Table and model

--- Main/apps/Save/income_models/variable.py
+++ Main/apps/Save/income_models/variable.py
@@ -5,9 +5,10 @@ class Variable_Cost(models.Model):
     name = models.CharField(max_length=155, verbose_name='Name', db_column='Name')
     value = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Value', db_column='Value')
     variable_type = models.CharField(max_length=2, verbose_name='Type', db_column='Type')
+
     is_active = models.BooleanField(default=True, verbose_name='Is Active', db_column='IsActive')
     id_transaction = models.IntegerField(unique=False, null=False, default=0, verbose_name='ID Transaction', db_column='IDTransaction')
-
+    DateAdded = models.DateField(auto_now_add=True, verbose_name='Date Added', db_column='DateAdded')
     def __str__(self):
         name = f"{self.name} ($ {self.value})"
         return name
